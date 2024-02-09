a6cc6263108adbbc89daf585aa490d994b19998a
DonghyeonKang <donghyeon3372@gmail.com>
2024-02-09 22:32:24 +0900

    Initialized Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # USG-TEAM-MEMBER-BACK
+
+---
+# Version
+
+Java : 17  
+Spring : 3.2.2  
+Build : gradle  
+packaging : jar  
+dependency : Spring Start Web, Spring Data Jpa, Lombok, MYSQL, H2, Validation, Spring Security
---
+++ src/test/java/com/usg/BookAndBeanstalkMember/BookAndBeanstalkMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usg.BookAndBeanstalkMember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAndBeanstalkMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
