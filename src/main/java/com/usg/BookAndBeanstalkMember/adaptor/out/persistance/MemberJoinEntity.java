e6cf2a6182f1ce77a0827bdb45692c2260a3803a
DonghyeonKang <donghyeon3372@gmail.com>
2024-02-10 19:44:35 +0900

    Feat : MemberJoinEntity 생성

---
+++ src/main/java/com/usg/BookAndBeanstalkMember/adaptor/out/persistance/MemberJoinEntity.java
@@ -0,0 +1,12 @@
+package com.usg.BookAndBeanstalkMember.adaptor.out.persistance;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberJoinEntity {
+
+    private String email;
+    private String password;
+}
