e4c606fc6935711d880f6a1faf949faac35e38b5
DonghyeonKang <donghyeon3372@gmail.com>
2024-02-10 19:36:00 +0900

    Feat : FindMember inputPort, outputPort 생성

---
+++ src/main/java/com/usg/BookAndBeanstalkMember/application/port/out/MemberFindOutputPort.java
@@ -0,0 +1,10 @@
+package com.usg.BookAndBeanstalkMember.application.port.out;
+
+import com.usg.BookAndBeanstalkMember.domain.Member;
+
+import java.util.Optional;
+
+public interface MemberFindOutputPort {
+
+    Optional<Member> findOne(String userId);
+}
