74ba2ce115c72c2313b0caf82c69861be9a649d2
DonghyeonKang <donghyeon3372@gmail.com>
2024-02-12 01:57:45 +0900

    Feat : Custom User Details Service 생성

---
+++ src/main/java/com/usg/BookAndBeanstalkMember/adaptor/in/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.usg.BookAndBeanstalkMember.adaptor.in.security;
+
+import com.usg.BookAndBeanstalkMember.application.usecases.FindOneMemberUseCase;
+import com.usg.BookAndBeanstalkMember.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final FindOneMemberUseCase findOneMemberUseCase;
+
+    @Override
+    public UserDetails loadUserByUsername(String insertedUserId) throws UsernameNotFoundException {
+        Optional<Member> findOne = findOneMemberUseCase.findOne(insertedUserId);
+        Member member = findOne.orElseThrow(() -> new UsernameNotFoundException("없는 회원입니다 ㅠ"));
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRoles())
+                .build();
+    }
+}
