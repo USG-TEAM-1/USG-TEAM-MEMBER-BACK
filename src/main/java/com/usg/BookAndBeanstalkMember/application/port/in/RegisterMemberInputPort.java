f3ebe97f0c675b709d67e80d74c6568a0216bd2b
Choi jun hyoek <chlwnsgur1214@naver.com>
2024-02-18 21:16:08 +0900

    Feat: 회원가입 시 kafka 로 회원 데이터를 보내는 로직 추 (닉네임 제외)

--- src/main/java/com/usg/BookAndBeanstalkMember/application/port/in/RegisterMemberInputPort.java
+++ src/main/java/com/usg/BookAndBeanstalkMember/application/port/in/RegisterMemberInputPort.java
@@ -1,6 +1,8 @@
 package com.usg.BookAndBeanstalkMember.application.port.in;
 
+import com.usg.BookAndBeanstalkMember.adaptor.out.kafka.MemberPublishDTO;
 import com.usg.BookAndBeanstalkMember.application.port.out.MemberJoinOutputPort;
+import com.usg.BookAndBeanstalkMember.application.port.out.MemberKafkaOutPort;
 import com.usg.BookAndBeanstalkMember.application.usecases.JoinMemberUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,24 @@ import org.springframework.stereotype.Service;
 public class RegisterMemberInputPort implements JoinMemberUseCase {
 
     private final MemberJoinOutputPort memberJoinOutputPort;
+    private final MemberKafkaOutPort memberKafkaOutPort;
 
     @Override
     public void join(String userid, String pw) {
 
-        memberJoinOutputPort.join(userid, pw);
+        Long savedMemberId = memberJoinOutputPort.join(userid, pw);
+
+        // nickname 추가 예정
+        MemberPublishDTO memberPublishDTO = createMemberPublishDTO(savedMemberId, userid, null);
+        memberKafkaOutPort.publishMember(memberPublishDTO);
+    }
+
+    private MemberPublishDTO createMemberPublishDTO(Long memberId, String email, String nickname) {
+        return MemberPublishDTO
+                .builder()
+                .memberId(memberId)
+                .email(email)
+//                .nickname()
+                .build();
     }
 }
