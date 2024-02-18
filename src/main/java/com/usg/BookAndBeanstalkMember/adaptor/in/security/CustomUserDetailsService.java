e86f6844642eb3c52413b9ca4439b9e17d165aa6
DonghyeonKang <donghyeon3372@gmail.com>
2024-02-19 01:04:04 +0900

    Feat: customUserDetailsService Bean 등록

--- src/main/java/com/usg/BookAndBeanstalkMember/adaptor/in/security/CustomUserDetailsService.java
+++ src/main/java/com/usg/BookAndBeanstalkMember/adaptor/in/security/CustomUserDetailsService.java
@@ -7,9 +7,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
